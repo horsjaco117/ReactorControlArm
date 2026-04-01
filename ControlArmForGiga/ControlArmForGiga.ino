ee8a94e7f44f6b06dbdf053eb792e0c29528fba1
horsjaco117 <jacob_horsley@live.com>
2026-04-01 14:09:42 -0600

    Working ethernet code.

--- ControlArmForGiga/ControlArmForGiga.ino
+++ ControlArmForGiga/ControlArmForGiga.ino
@@ -2,6 +2,15 @@
 #include <SPI.h>
 #include <Ethernet.h>
 
+//Ethernet Communication
+//MAC address
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+// IP address
+IPAddress ip(134, 50, 51, 77);
+
+EthernetServer server(80);
+
 //All Variables
 //Analog input variables---------------------------------------------------------
 int positionSetPin = A0;      //The position set voltage wire needs to go to A0
@@ -104,10 +113,37 @@ void setup() {//Digital Input Setup
   //Serial setup------------------------------------------
   Serial1.begin(9600);       //Tx Pin        
   Serial.begin(9600);       // USB debugging
+
+  while(!Serial) {
+    ;//wait for serial port to connect. Needed for native USB port only. 
+
+  }
+  Serial.println("Ethernet WebServerExample");
   //Serial1.begin(115200, SERIAL_8N1);  // TX1 output 1, 9600 baude, 8 bit packets, no parity bit
+
+  //Ethernet Setup
+  Ethernet.begin(mac, ip);
+
+ // Check for Ethernet hardware present
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    while (true) {
+      delay(1); // do nothing, no point running without Ethernet hardware
+    }
+  }
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
+  }
+
+  // start the server
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+
 }
 
 void loop() {
+
   bool currentScramReading = digitalRead(scramPin);
   bool currentPowerReading = digitalRead(powerPin);
   bool currentMagnetReading = digitalRead(electromagnetPin);
@@ -356,6 +392,93 @@ void loop() {
     // Serial.write(highByte(rotaryKnob2Read)); //For the other knob of the control panel
     // Serial.write(lowByte(rotaryKnob2Read));
     // Serial.write(0b00100100);
+
+    //Listen for Ethernet CLients
+
+    // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 60");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          client.println("<main id=\"main\" class=\"main-content\">"); //add some titles and contect classifications
+          /*build the table****************************/
+          client.println("<TABLE BORDER=\"5\" WIDTH=\"50%\" CELLPADDING=\"4\" CELLSPACING=\"3\">");
+          client.println("<TR>");
+          client.println("<TH COLSPAN=\"2\"><BR><H3>SENSOR DATA</H3>");
+          client.println("</TH>");
+          client.println("</TR>");
+          client.println("<TR>");
+          client.println("<TH>CHANNEL</TH>");
+          client.println("<TH>VALUE</TH>");
+          client.println("</TR>");
+          /*******************************************/
+          // output the value of each analog input pin to the table
+          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+            int sensorReading = analogRead(analogChannel); //read analogs
+            /*Add the channel to the table with the class identifier*/
+            client.println("<TR ALIGN=\"CENTER\">");
+            client.print("<TD class=\"chan\">");
+            client.print(analogChannel);
+            client.println("</TD>");
+            /*******************************************************/
+            /*Add the coorosponding value to the table*************/
+            client.print("<TD class=\"value\">");
+            client.print(sensorReading);
+            client.println("</TD>");
+            client.println("</TR>");
+            /******************************************************/
+          }
+          /*End the HTML****************/
+          client.println("</TABLE>");
+          client.println("</main>");
+          client.println("</html>");
+          /***************************/
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+
+    //Example of a Json sending byte
+     static unsigned long lastSend = 0;
+  if (millis() - lastSend > 5000) {
+    EthernetClient outgoing;
+    if (outgoing.connect(piIP, 6006)) {
+      JsonDocument doc;
+      doc["ard_val"] = analogRead(A0);
+      serializeJson(doc, outgoing);
+      outgoing.stop();
+    }
+    lastSend = millis()
   }
   }
+}
 
